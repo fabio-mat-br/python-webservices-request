0d7dc6c573ca16178438293c544314ffd332e71c
Fabio Fogliarini Brolesi <fbrolesi@cit>
2016-11-04 22:30:06 -0200

    Some adjusts

--- webservices_requests.py
+++ webservices_requests.py
@@ -12,20 +12,22 @@ def access(url, method, payload, wrong_params):
         new_palyoad = copy.copy(payload)
         new_params = copy.copy(wrong_params)
         new_params.append(payload[key])
+        q = None
         for i in xrange(0, len(new_params)):
+            current_result = None
             new_palyoad[key] = new_params[i]
             start = time.time()
-            q = requests.request(method, url, params = payload)
-            done = time.time()
-            elapsed = done - start
+            print new_palyoad
+            q = requests.request(method, url, params = new_palyoad)
             current_result = {
                 'url':     url,
                 'method':  method,
                 'start':   start,
-                'elapsed': elapsed,
+                'elapsed': q.elapsed,
                 'header':  q.headers,
                 'status':  q.status_code,
-                'body':    q.text
+                'body':    q.text,
+                'payload':  q.url
             }
             results.append(current_result)
     return results
